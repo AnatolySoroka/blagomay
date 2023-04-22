8ade054ad5c4ccd18cb63752a025cdd2bd9c2629
AnatolySoroka <toliksoroka66@gmail.com>
2023-04-22 09:06:23 +0300

    ttt

--- js/main.js
+++ js/main.js
@@ -164,6 +164,46 @@ $(document).ready(function () {
 
 //////
 
+// 2. This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+let player;
+
+$('.to-play').click(function () {
+
+  let btn = $(this),
+    videoID = btn.data('video'),
+    playerID = btn.data('id');
+
+
+  player = new YT.Player(playerID, {
+    playerVars: {
+      'autoplay': 0,
+      'conttrols': 1,
+      'playsinline': 1,
+    },
+    videoId: videoID,
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+});
+
+function onPlayerReady(event) {
+  let video = event.target.h;
+  $(video).siblings('.to-play').addClass('removed');
+  event.target.playVideo();
+}
+
+
+
+
 // $(document).ready(function () {
 //   $(".video-items").slick({
 //     centerPadding: "0px",
